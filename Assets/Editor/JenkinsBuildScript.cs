b0e1de7254fd51d21337f5a55986e1320452526c
lklab <alexeibatic@gmail.com>
2025-04-23 12:52:18 +0900

    젠킨스 설정

---
+++ Assets/Editor/JenkinsBuildScript.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+using UnityEditor;
+
+class JenkinsBuildScript
+{
+    static string[] SCENES = FindEnabledEditorScenes();
+
+    static void PerformBuild()
+    {
+        BuildPipeline.BuildPlayer(FindEnabledEditorScenes(), "Builds/Windows/MyGame.exe", BuildTarget.StandaloneWindows, BuildOptions.None);
+    }
+
+    private static string[] FindEnabledEditorScenes()
+    {
+        List<string> EditorScenes = new List<string>();
+        foreach (EditorBuildSettingsScene scene in EditorBuildSettings.scenes)
+        {
+            if (!scene.enabled) continue;
+            EditorScenes.Add(scene.path);
+        }
+        return EditorScenes.ToArray();
+    }
+}
